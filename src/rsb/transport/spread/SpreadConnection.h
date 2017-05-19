fb774a0e93a4f9947285f320f294b7e92600652b
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-05-19 12:58:20 +0200

    Fixed(?) Spread thread-safety issue in src/rsb/transport/spread/SpreadConnection.{h,cpp}
    At least for sending messages, the Spread client library does not seem
    to be thread-safe on win32 (despite what the documentation says).
    
    This fix seems to work but has only been verified experimentally. In
    particular, it is not clear, whether all other combinations of Spread
    client API functions are thread-safe.
    
    fixes #2386
    
    * src/rsb/transport/spread/SpreadConnection.{h,cpp} (header): updated
      copyright
      (SpreadConnection::mutex): new member; for critical section in send
      on win32
      (SpreadConnection::send): turned Spread client API calls into
      critical section on win32

--- src/rsb/transport/spread/SpreadConnection.h
+++ src/rsb/transport/spread/SpreadConnection.h
@@ -3,7 +3,7 @@
  * This file is part of the rsb-spread project
  *
  * Copyright (C) 2010 by Sebastian Wrede <swrede at techfak dot uni-bielefeld dot de>
- * Copyright (C) 2013, 2015 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ * Copyright (C) 2013, 2015, 2017 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
  *
  * This file may be licensed under the terms of the
  * GNU Lesser General Public License Version 3 (the ``LGPL''),
@@ -30,6 +30,9 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 #include <boost/thread/condition.hpp>
+#if defined WIN32 // see comment in SpreadConnection::send
+#include <boost/thread/mutex.hpp>
+#endif
 
 #include <rsc/logging/Logger.h>
 
@@ -189,6 +192,10 @@ private:
      * Number of message sent via this connection.
      */
     unsigned long msgCount;
+
+#if defined WIN32 // see comment in SpreadConnection::send
+    boost::mutex mutex;
+#endif
 };
 
 typedef boost::shared_ptr<SpreadConnection> SpreadConnectionPtr;
